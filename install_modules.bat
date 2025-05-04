1cd83b4d00f0f165954aacf22227e4b589074b03
matiasxgod <84541057+matiasxgod@users.noreply.github.com>
2025-05-05 00:21:54 +0300

    Delete install_modules.bat

--- install_modules.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-color 3
-
-echo Gerekli moduller yukleniyor...
-npm install discord.js axios @discordjs/rest discord-api-types dotenv
-
-echo Islemler tamamlandi. Enter tusuna basarak kapanabilir.
-pause
