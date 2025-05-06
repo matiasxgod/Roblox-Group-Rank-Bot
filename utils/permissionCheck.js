a7493813c01148f2cec09d60d65910908913eecc
matiasxgod <84541057+matiasxgod@users.noreply.github.com>
2025-05-06 17:56:28 +0300

    Delete utils directory

--- utils/permissionCheck.js
+++
@@ -1,7 +0,0 @@
-const ALLOWED_ROLES = ["Rütbelendirme"];
-
-function checkPermission(member) {
-    return member.roles.cache.some(role => ALLOWED_ROLES.includes(role.name));
-}
-
-module.exports = { checkPermission };
